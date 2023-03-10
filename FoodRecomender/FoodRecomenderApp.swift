04cd8467b26aa093aaf89788348d5c82bea11cab
Tim Yoon <stimyoon@gmail.com>
2023-03-09 19:12:17 -0500

    Initial Commit

---
+++ FoodRecomender/FoodRecomenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodRecomenderApp.swift
+//  FoodRecomender
+//
+//  Created by Tim Yoon on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct FoodRecomenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
